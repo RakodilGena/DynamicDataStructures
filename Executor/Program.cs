d96dd744299fa0d8e12e3dce64b80fa1846a9846
RakodilGena <32132923+RakodilGena@users.noreply.github.com>
2022-10-15 19:43:27 +0300

    Single Linked List Realisation
    Add indexer to ILinkedList interface.
    Add realisation of ILinkedList as SingleLinkedList.
    
    Add Execution func at Executor for tests on any of ILinkedList subclasses.

--- Executor/Program.cs
+++ Executor/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace Executor
+﻿using DynamicDataStructuresLib.LinkedLists;
+
+namespace Executor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Dynamic Data Structures!\r\n\r\n");
+
+            Console.WriteLine("Test of Single Linked List\r\n");
+            var singleList = new SingleLinkedList<int>();
+            LinkedListExecution(singleList);
+        }
+
+
+        private static void LinkedListExecution(ILinkedList<int> linkedList)
+        {
+            linkedList.Print();
+
+            var newRange = new[] { 1, 2, 3, 4 };
+            linkedList.AddRange(newRange);
+            linkedList.Print();
+
+            linkedList.RemoveAt(2);
+            linkedList.Print();
+
+            Console.WriteLine($"Elements[2] is {linkedList.GetElement(2)}\r\n");
+            linkedList[2] = -80;
+            Console.WriteLine($"Elements[2] is {linkedList.GetElement(2)}\r\n");
+
+            linkedList = new SingleLinkedList<int>(5, 6, 7, 8);
+            linkedList.Print();
+
+            linkedList.AddFirst(1);
+            linkedList.Print();
+
+            linkedList.AddLast(20);
+            linkedList.Print();
+
+            linkedList.Insert(3, -55);
+            linkedList.Print();
+
+            linkedList.RemoveAt(2);
+            linkedList.Print();
+
+            linkedList.RemoveAt(2);
+            linkedList.Print();
+
+            Console.WriteLine("First = " + linkedList.GetFirst());
+            Console.WriteLine("Last = " + linkedList.GetLast() + "\r\n");
+
+            linkedList.AddRange(new[] { -56, 4, 45, 5243, 1312, 23, 23, 4444 });
+            linkedList.Print();
         }
     }
 }
